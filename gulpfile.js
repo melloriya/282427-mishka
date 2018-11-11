aa28c5e92e6981589c5d9e1dc96adaad59152462
Ильмира <melloriya@gmail.com>
2018-11-11 23:24:22 +0300

    Правки по Пиксельперфект

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 "use strict";
-
+var del = require("del");
 var gulp = require("gulp");
-var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
+var imagemin = require("gulp-imagemin");
+var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
+var posthtml = require("gulp-posthtml");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
 var server = require("browser-sync").create();
+var rename = require("gulp-rename");
 
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest("source/img"));
+});
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
@@ -14,13 +27,20 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
-
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+  .pipe(posthtml())
+  .pipe(gulp.dest("build"));
+});
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -28,7 +48,30 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html" , gulp.series("html" , "refresh"));
 });
-
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("clean", function () {
+ return del("build");
+});
+gulp.task("copy", function () {
+ return gulp.src([
+ "source/fonts/**/*.{woff,woff2}",
+ "source/img/**",
+ "source/js/**",
+ "source/css/normalize.css"
+ ], {
+ base: "source"
+ })
+ .pipe(gulp.dest("build"));
+});
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "html"
+));
+gulp.task("start", gulp.series("build", "server"));
--- source/js/modal.js
+++ source/js/modal.js
@@ -1,6 +1,11 @@
-var link = document.querySelector(".button--modal");
-var popup = document.querySelector(".modal");
+var link = document.querySelector(".button--modal"),
+    over = document.querySelector(".modal__overlay"),
+    popup = document.querySelector(".modal");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal__open");
+});
+over.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal_open");
 });
